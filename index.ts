358522d030c13725c2e2e3fe7b3f2516a4375f35
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-10-08 14:04:12 +0200

    changes

--- index.ts
+++ index.ts
@@ -2,6 +2,20 @@ import axios from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
+interface Todo {
+  id: number;
+  title: string;
+  finished: boolean;
+}
+
 axios.get(url).then((response) => {
-  console.log(response.data), 'FUCK YOUUUUUUUUU';
+  const todo = response.data;
+  const ID = todo.id;
+  const title = todo.title;
+  const completed = todo.finished;
+
+  console.log(`
+    The ToDo with ID ${ID} 
+    has a title of ${title}
+    and is it finished? ${completed}`);
 });
